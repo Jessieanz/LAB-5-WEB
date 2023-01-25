5a0a2c150a4f9dc3dca291fbf5bcd9f86f940165
Jessieanz <A00825210@itesm.mx>
2023-01-24 23:31:18 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(".agregar").click(AddItem)
+$(".Lista").on("click", ".check", checar)
+$(".Lista").on("click", ".delete", eliminar)
+
+function AddItem(e){
+    e.preventDefault()
+    let newItem = $("#newText").val()
+    $(".Lista").append(` <div class=recuadros> <li> <p id="item">${newItem}</p> <p></p>
+    <button class="check" type="submit">Check</button> 
+    <button class="delete" type="submit">Delete</button> <p></p> </li> </div> `)
+}
+
+function checar(){
+    let sibling_button = $(this).siblings("#item")
+    sibling_button.toggleClass('checked')
+}
+
+function eliminar(){
+    let grandpa_div = $(this).parent().parent()
+    grandpa_div.remove()
+}
+
